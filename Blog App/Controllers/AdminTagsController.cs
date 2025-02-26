99bc825ca1ff084b21f6e2045829f875ee99ce20
Karan Panda <karanpanda1206@gmail.com>
2025-02-26 23:56:56 +0530

    Added Add Tags View

---
+++ Blog App/Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_App.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
