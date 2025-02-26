297545eaf32a9e02db44989f14726a16cc84760a
arjun <arjun@FANGBEAST>
2025-02-26 23:17:06 +0530

    Add project files.

---
+++ Blog App/Controllers/Domain/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog_App.Controllers.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string PageTitle { get; set; }
+
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
---
+++ Blog App/Data/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Blog_App.Controllers.Domain;
+
+
+namespace Blog_App.Data
+{   
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+       
+    }
+}
